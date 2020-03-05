c81124252bb6781fc86e9ec63137af78d58d789f
Dane Burns <daneb91@gmail.com>
2020-03-05 10:22:47 -0700

    fixed heroku

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,9 +16,9 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
   });
 
-  app.get("/api/notes", function(req, res) {
-    // console.log(res.body);
-  });
+  // app.get("/api/notes", function(req, res) {
+  //   // console.log(res.body);
+  // });
   
 
   app.listen(PORT, function() {
